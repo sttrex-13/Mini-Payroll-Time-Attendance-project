d788953c6ced3dd60d1867226adc18af500a46b5
sttrex-13 <sutee.sttrex13@odds.team>
2026-03-07 16:44:13 +0700

    feat: add validate create employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :salary, presence: true
+  validates :position, presence: true
 end
